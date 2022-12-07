8a0ec7e7ec2fbea670d2dbf1a567c3f81ced1889
Shubham More <97827118+shubhamore@users.noreply.github.com>
2022-12-07 08:24:36 +0530

    Add files via upload

---
+++ q20.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+class maxheap{
+    int[] insert(int[] arr, int n, int key){
+        arr[n] = key;
+        int i = n;
+        while (i>0){
+            int parent = (i-1)/2;
+            if(arr[parent]<arr[i]){
+                int temp = arr[parent];
+                arr[parent] = arr[i];
+                arr[i] = temp;
+                i = parent;
+                System.out.println(Arrays.toString(arr));
+            }
+            else{
+                return arr;
+            }
+        }
+        return arr;
+    }
+    void heapify(int arr[],int n,int i){
+        int maxelement = i;
+        int left = 2*i+1;
+        int right = 2*i+2;
+        if(left<=n && arr[left]>arr[maxelement]){
+            maxelement= left;
+        }
+        if(right<=n && arr[right]>arr[maxelement]){
+            maxelement = right;
+        }
+        if(maxelement!=i){
+            int temp = arr[i];
+            arr[i] = arr[maxelement];
+            arr[maxelement] = temp;
+            heapify(arr,n,maxelement);
+        }
+
+    }
+    void heapsort(int arr[]){
+        int n = arr.length;
+        for(int i=n-1;i>=0;i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr,i-1,0);
+        }
+
+    }
+}
+public class q20 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number of elements ");
+        int n = sc.nextInt();
+        maxheap heap = new maxheap();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements ");
+        for(int i=0;i<n;i++){
+            System.out.print("Enter element "+(i+1)+" ");
+            arr[i] = sc.nextInt();
+            arr = heap.insert(arr,i,arr[i]);
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Sorting Started");
+//        int arr2[]= {20,35,9,26,49,78,2,46};
+//        heap.heapify(arr2,n,0);
+        heap.heapsort(arr);
+        System.out.print("Final sorted array :   ");
+        System.out.println(Arrays.toString(arr));
+    }
+}
